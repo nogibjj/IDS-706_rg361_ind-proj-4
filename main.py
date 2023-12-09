a42db3ab3bd73c45447c54b4228af7396dc3ec24
Revanth Chowdary <46459100+revanth7667@users.noreply.github.com>
2023-12-09 02:29:05 +0000

    added basic API call

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/grammarly/coedit-large"
+headers = {"Authorization": f"Bearer {'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'}"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+	
+output = query({
+	"inputs": "Fix the grammar: When I grow up I start to understand what he said is quite right.",
+})
+print(output[0]["generated_text"])
